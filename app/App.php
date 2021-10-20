78f7ac76bd5f9496efbad9cbd3ba964f98cbff73
RaphaelStn <raphaelstacino@gmail.com>
2021-10-20 09:34:30 +0200

    ajout de commentaire pour mieux apprehender

--- app/App.php
+++ app/App.php
@@ -8,22 +8,22 @@ class App {
     public $title = 'J. Forteroche';
     private $db_instance;
 
-    public static function load(){
+    public static function load(){ // load les Autoloader et le sessionstart().
         session_start();
         require ROOT . '/app/Autoloader.php';
         App\Autoloader::register();
         require ROOT . '/Core/Autoloader.php';
         Core\Autoloader::register();
     }
-    public static function getInstance() { // On initialise via une unique instance static et un 'singleton'
+    public static function getInstance() { // On initialise via une unique instance static et un 'singleton'.
         if(self::$_instance === null){ 
             self::$_instance = new App();
         }
         return self::$_instance;
     }
-    public function getTable($name) { // On initialise les DB selon leur nom
+    public function getTable($name) { // On initialise les DB selon leur nom, et en injectant la config de la db(db_pass etc)
         $class_name = '\\App\\Table\\' . ucfirst($name) . 'Table';
-        return new $class_name($this -> getDb());
+        return new $class_name($this -> getDb()); // On retourne par exemple \App\Table\BilletTable(db_instance) où db_instance est MysqlDatabase('blog','localhost'etc).
     }
     public function getDb() { // On init la db via le fichier config
         $this -> config = Config::getInstance(ROOT . '\config\config.php');
@@ -34,13 +34,13 @@ class App {
             return $this -> db_instance;
         }
     }
-    public function getTitle() {
+    public function getTitle() { // getter pour le titre.
         return $this -> title;
     }
-    public function setTitle($title) {
+    public function setTitle($title) { // setter pour le titre.
         return  $this -> title = $title . ' | ' . $this -> title;
     }
-    public  function NotFound() {
+    public  function NotFound() { // 404 not found.
         header ("HTTP/1.0 404 Not Found");
         header('Location:index.php?action=404');
     }
